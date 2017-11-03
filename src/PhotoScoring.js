cc5c720153c588e9a3fedc882a440f2a22c819f4
tianxiangbing <55342775@qq.com>
2017-11-03 18:44:29 +0800

    temp

--- src/PhotoScoring.js
+++ src/PhotoScoring.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { view as Photo } from './photo/';
 import { view as Scoring } from './scoring/';
+import html2canvas from 'html2canvas';
 
 function PhotoScoring() {
     return (
-        <div >
-        <h1>人要有自信</h1>
+        <div onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
+            <h1>人要有自信</h1>
             <div className="bg" />
             <div className="content">
                 <Photo />
@@ -13,6 +14,20 @@ function PhotoScoring() {
             </div>
         </div>
     )
+}
+let timer = undefined;
+const onTouchStart = (e) => {
+    timer = setTimeout(() => {
+        //长按
+        html2canvas(document.body, {
+            onrendered: function (canvas) {
+                document.body.appendChild(canvas);
+            }
+        });
+    }, 1000)
+}
+const onTouchEnd = (e) => {
+    timer && clearTimeout(timer);
 }
 
 export default PhotoScoring
